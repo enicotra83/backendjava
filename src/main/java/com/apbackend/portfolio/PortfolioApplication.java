42217e6fda623e0fe6f0efcba974cba87d7eff11
Eliana <enicotra83@gmail.com>
2022-11-01 00:30:52 -0300

    modificando cors a ver si funciona

--- src/main/java/com/apbackend/portfolio/PortfolioApplication.java
+++ src/main/java/com/apbackend/portfolio/PortfolioApplication.java
@@ -1,5 +1,7 @@
 package com.apbackend.portfolio;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
